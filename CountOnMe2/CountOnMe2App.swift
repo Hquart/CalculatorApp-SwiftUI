31fc6ae8ed5e0a81d195b41ff5f8e55dc68258c3
Hquart <hquart@icloud.com>
2021-01-08 11:05:14 +0100

    Initial Commit

---
+++ CountOnMe2/CountOnMe2App.swift
@@ -0,0 +1,17 @@
+//
+//  CountOnMe2App.swift
+//  CountOnMe2
+//
+//  Created by Naji Achkar on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountOnMe2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
